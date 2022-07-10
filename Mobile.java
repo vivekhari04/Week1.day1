9efaac95390d8833d8c050b6984444d0c18ef29e
vivekhari04 <109015733+vivekhari04@users.noreply.github.com>
2022-07-10 12:07:24 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Mobile {
+	public void makeCall() {
+	 long mobNumber=95854789553L;
+	  System.out.println(mobNumber);
+	 
+	}
+	public void sendMsg() {
+	 String message = "Hi how are you?";
+	 System.out.println(message);
+	}
+	//TODO Auto-generated method stub
+
+	public static void main(String[] args){
+		String statement  = "This is my mobile";
+		System.out.println(statement);
+}
+}
